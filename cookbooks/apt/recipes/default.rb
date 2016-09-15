4142ebe855bfcd3fa788f5d2e1d43788a54c4c87
Alejandro <alemail@gmail.com>
2016-09-15 17:31:04 +0200

    recipes added, georush bugfix

--- cookbooks/apt/recipes/default.rb
+++ cookbooks/apt/recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+execute "apt-get update" do
+   command "apt-get update"
+   end
--- cookbooks/georush/recipes/default.rb
+++ cookbooks/georush/recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+include_recipe "apt"
+
+package 'python3' do
+  action :install
+end
+
+package 'geoip-bin' do
+  action :install
+end
+
+package 'geoip-database' do
+  action :install
+end
+
+cookbook_file "/root/georush.py" do
+  source "georush.py"
+  mode "0774"
+end
